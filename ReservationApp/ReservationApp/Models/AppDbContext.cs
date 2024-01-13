836cd5e2f9d939f9d62457aad57899994f60860a
unknown <gosgie02@wp.pl>
2024-01-13 14:23:01 +0100

    implement user registration functionality

--- ReservationApp/ReservationApp/Models/AppDbContext.cs
+++ ReservationApp/ReservationApp/Models/AppDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ReservationApp.Models
 {
-    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
---
+++ ReservationApp/ReservationApp/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationApp.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set;}
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set;}
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Hasła nie są takie same!")]
+        public string ConfirmPassword { get; set;}
+    }
+}
